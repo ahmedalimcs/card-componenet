5ec8ff206b06f848ee7b91b02917ffbd1f3de80f
ahmedalimcs <ahmedalimcs21@gmail.com>
2021-10-22 19:11:57 +0500

    cards commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import mydata from "./Head";
 
 function App() {
+  // const [name, SetName] = useState("");
+  const [category,setCategory] = useState('')
+  const [model,modelno] = useState('')
+
+  // const Category=()=>{
+  //   mydata.filter(ind,e)
+    
+  // }
+  //    setCategory()
+  //    console.log(category)
+
+
+  // const Add = () => {
+  //   if (SetName == "") {
+  //   } else {
+  //     SetName("java");
+  //     console.log(name);
+  //   }
+  // };
+const categoryHandler =(name) =>{
+  setCategory(name)
+}
+
+ const Foo =(modelno)=>{
+  setModelno(modelno)
+ }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div className="container mt-5 p-5">
+      <div className="row">
+        {mydata.filter((elem,index)=>elem.category == modelno).map((newelem,ind)=>{
+          return <div className="col-lg-4 mb-5">
+           <div className="card">
+             <div className="card-body">
+               <h2>{newelem.modelno}</h2>
+               <p>{newelem.name}</p>
+               <h5>{newelem.category}</h5>
+             </div>
+           </div>
+         </div>
+        })}
+      </div>
     </div>
+     
+     <div className="container mb-5 ">
+      <button className="btn btn-warning btn-lg " onClick={()=>categoryHandler('1558')}> Category1</button>
+      <button className="btn btn-warning btn-lg " onClick={()=>categoryHandler('1558')} >Category2</button>
+      <button className="btn btn-warning btn-lg " onClick={()=>categoryHandler('1559')} >Category3</button>
+      <button className="btn btn-warning btn-lg " onClick={()=>categoryHandler('1660')} >Category4</button>
+      <button></button>
+     </div>
+    </>
   );
 }
 
---
+++ src/Head.js
@@ -0,0 +1,14 @@
+const mydata = [
+  { name: "ahmed", category: "kenwood", modelno: "1558" },
+  { name: "ali", category: "haeir", modelno: "1558" },
+  { name: "jaffar", category: "dawlance", modelno: "1559" },
+  { name: "usama", category: "dawlance", modelno: "1660" },
+  { name: "saeed", category: "papel", modelno: "1661" },
+  { name: "umair", category: "haeir", modelno: "1662" },
+  { name: "kashif", category: "haeir", modelno: "1663" },
+  { name: "saad", category: "dsss", modelno: "1664" },
+  { name: "umer", category: "loop", modelno: "1665" },
+  { name: "hassan", category: "haeir", modelno: "1666" },
+];
+
+export default mydata;
